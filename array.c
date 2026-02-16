6ab85ef0c0a58a8cbe4e2045e918ae83c3d23119
LAB64_USER ON WS20 <L64X10W20@LabHCI.net>
2026-02-16 12:54:45 +0800

    done implementing funcs

--- array.c
+++ array.c
@@ -1,20 +1,6 @@
 #include "array.h"
 #include <stdio.h>
 
-void display(int arr[], int count)
-{
-    printf("[ ");
-    int i;
-    for (i = 0; i < count; i++)
-    {
-        printf("%d", arr[i]);
-
-        if (i < count - 1)
-            printf(", ");
-    }
-    printf(" ]");
-}
-
 void insertBeginning(int arr[], int *count, int value)
 {
     int i;
@@ -83,11 +69,91 @@ void deleteAllOccurrences(int arr[], int *count, int value)
 {
     int index;
     for (index = 0; index < *count; index++)
-    {
         if (arr[index] == value)
         {
             deleteByValue(arr, count, value);
             index--;
         }
+}
+
+void deleteSecondSmallest(int arr[], int *count)
+{
+    int cpyIndex;
+    int cpyArr[*count];
+    for (cpyIndex = 0; cpyIndex < *count; cpyIndex++)
+        cpyArr[cpyIndex] = arr[cpyIndex];
+
+    int sortIndex;
+    int isSwapped = 1;
+    while (isSwapped)
+    {
+        isSwapped = 0;
+        for (sortIndex = 0; sortIndex < *count - 1; sortIndex++)
+            if (cpyArr[sortIndex] > cpyArr[sortIndex + 1])
+            {
+                isSwapped = 1;
+
+                int temp = cpyArr[sortIndex + 1];
+                cpyArr[sortIndex + 1] = cpyArr[sortIndex];
+                cpyArr[sortIndex] = temp;
+            }
     }
+
+    int secondSmall = cpyArr[1];
+
+    printf("Second Smallest: %d\n", secondSmall);
+    deleteByValue(arr, count, secondSmall);
+}
+
+void deleteSecondLargest(int arr[], int *count)
+{
+    int cpyIndex;
+    int cpyArr[*count];
+    for (cpyIndex = 0; cpyIndex < *count; cpyIndex++)
+        cpyArr[cpyIndex] = arr[cpyIndex];
+
+    int sortIndex;
+    int isSwapped = 1;
+    while (isSwapped)
+    {
+        isSwapped = 0;
+        for (sortIndex = 0; sortIndex < *count - 1; sortIndex++)
+            if (cpyArr[sortIndex] > cpyArr[sortIndex + 1])
+            {
+                isSwapped = 1;
+
+                int temp = cpyArr[sortIndex + 1];
+                cpyArr[sortIndex + 1] = cpyArr[sortIndex];
+                cpyArr[sortIndex] = temp;
+            }
+    }
+
+    int secondLarge = cpyArr[*count - 2];
+
+    printf("Second Largest: %d\n", secondLarge);
+    deleteByValue(arr, count, secondLarge);
+}
+
+void display(int arr[], int count)
+{
+    printf("[ ");
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        printf("%d", arr[i]);
+
+        if (i < count - 1)
+            printf(", ");
+    }
+    printf(" ]");
+}
+
+int search(int arr[], int count, int value)
+{
+    int index;
+    for (index = 0; index < count; index++)
+        if (arr[index] == value)
+            return index + 1;
+
+    return -1;
 }
--- main.c
+++ main.c
@@ -7,47 +7,76 @@
 int main(int argc, char *argv[])
 {
 
-	int count = 10;
-	int arr[100] = {1, 6, 8, 9, 4, 5, 7, 2, 3, 10};
+	// int count = 10;
+	// int arr[100] = {1, 6, 8, 9, 4, 5, 7, 2, 3, 10};
 
-	printf("Main array: ");
-	display(arr, count);
-	printf("\n");
+	// printf("Main array: ");
+	// display(arr, count);
+	// printf("\n");
 
-	printf("\nInserting 10 at beginning: ");
-	insertBeginning(arr, &count, 10);
-	display(arr, count);
-	printf("\n");
+	// printf("\nInserting 10 at beginning:\n");
+	// display(arr, count);
+	// printf("\n");
+	// display(arr, count);
+	// insertBeginning(arr, &count, 10);
+	// display(arr, count);
+	// printf("\n");
 
-	printf("\nInserting 69 at end: ");
-	insertEnd(arr, &count, 69);
-	display(arr, count);
-	printf("\n");
+	// printf("\nInserting 69 at end:\n");
+	// display(arr, count);
+	// printf("\n");
+	// insertEnd(arr, &count, 69);
+	// display(arr, count);
+	// printf("\n");
 
-	printf("\nInserting 420 at position 2 (index 1): ");
-	insertAtPosition(arr, &count, 2, 420);
-	display(arr, count);
-	printf("\n");
+	// printf("\nInserting 420 at position 2 (index 1):\n");
+	// display(arr, count);
+	// printf("\n");
+	// insertAtPosition(arr, &count, 2, 420);
+	// display(arr, count);
+	// printf("\n");
 
-	printf("\nInserting 123 at after 6: ");
-	insertAfterValue(arr, &count, 6, 123);
-	display(arr, count);
-	printf("\n");
+	// printf("\nInserting 123 at after 6:\n");
+	// display(arr, count);
+	// printf("\n");
+	// insertAfterValue(arr, &count, 6, 123);
+	// display(arr, count);
+	// printf("\n");
 
-	printf("\nDeleting at Pos 7 (index 6): ");
-	deleteAtPosition(arr, &count, 7);
-	display(arr, count);
-	printf("\n");
+	// printf("\nDeleting at Pos 7 (index 6):\n");
+	// display(arr, count);
+	// printf("\n");
+	// deleteAtPosition(arr, &count, 7);
+	// display(arr, count);
+	// printf("\n");
 
-	printf("\nDeleting at first 10: ");
-	deleteByValue(arr, &count, 10);
-	display(arr, count);
-	printf("\n");
+	// printf("\nDeleting at first 10:\n");
+	// display(arr, count);
+	// printf("\n");
+	// deleteByValue(arr, &count, 10);
+	// display(arr, count);
+	// printf("\n");
 
-	printf("\nDeleting all occurences of 420: ");
-	deleteAllOccurrences(arr, &count, 420);
-	display(arr, count);
-	printf("\n");
+	// printf("\nDeleting all occurences of 420:\n");
+	// display(arr, count);
+	// printf("\n");
+	// deleteAllOccurrences(arr, &count, 420);
+	// display(arr, count);
+	// printf("\n");
+
+	// printf("\nDeleting the second smallest:\n");
+	// display(arr, count);
+	// printf("\n");
+	// deleteSecondSmallest(arr, &count);
+	// display(arr, count);
+	// printf("\n");
+
+	// printf("\nDeleting the second smallest:\n");
+	// display(arr, count);
+	// printf("\n");
+	// deleteSecondLargest(arr, &count);
+	// display(arr, count);
+	// printf("\n");
 
 	return 0;
 }
