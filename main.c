ffa1d6dcf8c1b67fd9ad88a65aa4a658a86f96e8
LAB64_USER ON WS20 <L64X10W20@LabHCI.net>
2026-02-16 13:23:37 +0800

    starting to user input

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "array.h"
 
+void printCurrentArr(int arr[], int count);
+void printBorderLine();
+
 #define MAX_COUNT 10
 
 int main(int argc, char *argv[])
@@ -78,6 +81,97 @@ int main(int argc, char *argv[])
 	// display(arr, count);
 	// printf("\n");
 
+	int count = 0;
+	int arr[MAX_COUNT];
+	int choice;
+
+	do
+	{
+		printBorderLine();
+
+		printCurrentArr(arr, count);
+		printf("\n");
+
+		printf("1. Insert an element at the beginning\n");
+		printf("2. Insert an element at the end\n");
+		printf("3. Insert an element at a specific position\n");
+		printf("4. Insert an element after a given value\n");
+		printf("5. Delete an element at a specific position\n");
+		printf("6. Delete the first occurrence of a value\n");
+		printf("7. Delete all occurrences of a value\n");
+		printf("8. Delete the second smallest element\n");
+		printf("9. Delete the second largest element\n");
+		printf("10. Search for an element\n");
+		printf("11.Display all elements\n");
+		printf("12. Exit\n");
+		printf("\n\n");
+
+		do
+		{
+			printf("Choice: ");
+			scanf(" %d", &choice);
+
+			if (choice < 1 || choice > 12)
+				printf("\nPlease input a valid choice. ");
+
+		} while (choice < 1 || choice > 12);
+
+		switch (choice)
+		{
+		case 1:
+		{
+			int val;
+
+			printf("Input value to insert: ");
+			scanf(" %d", &val);
+
+			insertBeginning(arr, &count, val);
+		}
+		break;
+
+		case 2:
+		{
+
+			int val;
+
+			printf("Input value to insert: ");
+			scanf(" %d", &val);
+
+			insertEnd(arr, &count, val);
+		}
+		break;
+
+		case 3:
+		{
+			int pos, val;
+
+			printf("Enter position to insert to (1 - %d): ", count);
+			scanf(" %d", &pos);
+
+			if (pos < 1 || pos > 12)
+			{
+				printf("Invalid input.");
+				break;
+			}
+
+			printf("Enter value to insert: ");
+			scanf(" %d", val);
+
+			insertAtPosition(arr, &count, pos, val);
+		}
+		break;
+
+		case 12:
+			exit(0);
+			break;
+
+		default:
+			printf("%d is a good value\n", choice);
+			break;
+		}
+
+	} while (1);
+
 	return 0;
 }
 
@@ -85,3 +179,19 @@ int main(int argc, char *argv[])
 //
 // display(arr, count);
 // printf("\n");
+
+void printCurrentArr(int arr[], int count)
+{
+	printf("Current array: ");
+	display(arr, count);
+	printf("\n");
+}
+
+void printBorderLine()
+{
+	int i;
+	for (i = 0; i < 50; i++)
+		printf("-");
+
+	printf("\n");
+}
